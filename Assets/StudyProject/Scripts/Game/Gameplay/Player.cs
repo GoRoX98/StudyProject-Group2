5e27ff6f1b47c2cff11f2fbab1b6752e29296f71
GoRoX98 <goga-grox@mail.ru>
2024-03-13 17:51:14 +0300

    Input Update
    + TouchScreen input
    - Отредактировано передвижение персонажа

--- Assets/StudyProject/Scripts/Game/Gameplay/Player.cs
+++ Assets/StudyProject/Scripts/Game/Gameplay/Player.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.LowLevel;
 
 [RequireComponent(typeof(PlayerInput))]
 public class Player : MonoBehaviour
@@ -13,9 +15,16 @@ public class Player : MonoBehaviour
     private PlayerInput _input;
     private InputAction _moveAction;
     private InputAction _jumpAction;
+    private InputAction _contact;
+    private InputAction _touch;
 
     private Gameplay _gameplay;
     private float _timer = 0f;
+    [Header("Move Settings")]
+    [SerializeField][Range(0, 1)] private float _speedIncrase = 1;
+    [SerializeField] private float _maxSpeed = 5;
+    private Vector2 _movement = new Vector2(0,0);
+    private float _currentSpeed = 0;
 
     private void Awake()
     {
@@ -23,6 +32,8 @@ public class Player : MonoBehaviour
         _input = GetComponent<PlayerInput>();
         _moveAction = _input.actions["Move"];
         _jumpAction = _input.actions["Jump"];
+        _contact = _input.actions["PrimaryContact"];
+        _touch = _input.actions["MoveTouch"];
     }
 
     private void OnEnable()
@@ -40,9 +51,44 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if (_input.currentControlScheme == "Mobile")
+        {
+            if (_contact.inProgress)
+            {
+                TouchState touch = _touch.ReadValue<TouchState>();
+                Vector2 startPos = touch.startPosition;
+                Vector2 currentPos = touch.position;
+
+                _movement = currentPos - startPos;
+                _movement.Normalize();
+            }
+            else
+                _movement = Vector2.zero;
+        }
+        else
+        {
+            _movement = _moveAction.ReadValue<Vector2>();
+        }
+
         if (Input.GetMouseButtonDown(0))
             Instantiate(_bulletPrefab, _gun.position, _bulletPrefab.transform.rotation);
         _timer += Time.deltaTime;
+
+        if (_movement.y != 0 || _movement.x != 0)
+        {
+            if (_currentSpeed < _maxSpeed)
+                _currentSpeed += _speedIncrase * Time.deltaTime;
+        }
+        else
+        {
+            _currentSpeed = 0;
+        }
+
+        if (_currentSpeed > 0)
+        {
+            transform.position += (transform.forward * _movement.y + transform.right * _movement.x) * _currentSpeed * Time.deltaTime;
+        }
+
         //Old movement
         /*
         if (_timer > 3f && Input.GetKeyDown(KeyCode.Space))
@@ -60,16 +106,8 @@ public class Player : MonoBehaviour
     //New Input System
     private void Move(InputAction.CallbackContext context)
     {
-        Vector2 move = context.ReadValue<Vector2>();
-
-        if (move == Vector2.up)
-            transform.position += transform.forward * _speed;
-        if (move == Vector2.down)
-            transform.position += -transform.forward * _speed;
-        if (move == Vector2.left)
-            transform.position += -transform.right * _speed;
-        if (move == Vector2.right)
-            transform.position += transform.right * _speed;
+        //_movement = context.ReadValue<Vector2>();
+        //print($"Vector {_movement}");
     }
 
     private void Jump(InputAction.CallbackContext obj)
